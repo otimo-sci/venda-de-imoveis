1ecaa6d59c7a855a728777beb35bed4fd85f2063
Francisco Andre Maculuve <franciscomaculuve002@gmail.com>
2023-07-17 11:10:05 +0200

    updating - venda de imoveis - v2

---
+++ main.js
@@ -0,0 +1,10 @@
+
+    var detail_hide = document.querySelector('.show-hide-btn');
+
+    detail_hide.addEventListener("click",function(){
+
+        var section_detail = document.querySelector('.properties-single-slideshow-info');
+    
+        section_detail.classList.add('active');
+
+    });
